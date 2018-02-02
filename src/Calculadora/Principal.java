071efbec0b66aa90bd10a17bbeba9ec1315b8bb4
her17138 <her17138@uvg.edu.gt>
2018-02-01 19:40:46 -0600

    Se le agregó la división dentro de 0

--- src/Calculadora/Principal.java
+++ src/Calculadora/Principal.java
@@ -36,7 +36,15 @@ public class Principal {
                 line = br.readLine();
             }
             String everything = sb.toString();
-            System.out.println(calc.operar(everything));
+            if (Double.isNaN(calc.operar(everything)))
+            {
+                System.out.println("Se ha realizado una división dentro de 0, lo cual no es matemáticamente válido.");
+            }
+            else
+            {
+                System.out.println(calc.operar(everything));
+            }
+            
         } 
         finally 
         {
